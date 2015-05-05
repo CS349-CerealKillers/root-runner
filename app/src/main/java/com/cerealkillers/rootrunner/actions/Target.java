48c9f09fc27ab0c31cb636b64d0629dc8bea68c6
Benjamin Daschel <coffeemaxed@gmail.com>
2015-05-05 10:22:34 -0700

    add some models

---
+++ app/src/main/java/com/cerealkillers/rootrunner/actions/Target.java
@@ -0,0 +1,7 @@
+package com.cerealkillers.rootrunner.actions;
+
+/**
+ * Created by ben on 4/28/15.
+ */
+public interface Target {
+}
---
+++ app/src/main/java/com/cerealkillers/rootrunner/gamemodels/Tool.java
@@ -0,0 +1,11 @@
+package com.cerealkillers.rootrunner.gamemodels;
+
+/**
+ * Created by ben on 4/28/15.
+ */
+public interface Tool extends Item{
+    /**
+     * //TODO maybe this method should take a parameter? Maybe not.
+     */
+    public void use();
+}
