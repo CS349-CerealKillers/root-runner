8b5a9bc936b7b28312ed882a88aa00c3bba9a50c
Benjamin Daschel <coffeemaxed@gmail.com>
2015-05-24 18:24:48 -0700

    make sub-factories for tools and concrete items

---
+++ app/src/main/java/com/cerealkillers/rootrunner/GameObjects/Items/ConcreteItemFactory.java
@@ -0,0 +1,13 @@
+package com.cerealkillers.rootrunner.GameObjects.Items;
+
+import org.andengine.extension.tmx.TMXObject;
+
+/**
+ * Created by Benjamin Daschel on 5/24/15.
+ */
+public class ConcreteItemFactory {
+
+    public static Item itemFromTmxObject(TMXObject tmxObject) {
+        return null;
+    }
+}
