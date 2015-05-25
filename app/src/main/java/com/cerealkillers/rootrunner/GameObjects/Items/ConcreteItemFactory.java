7d5c1c9bb2f62f7b988be0016d939f519e85d1fb
Benjamin Daschel <coffeemaxed@gmail.com>
2015-05-24 18:33:44 -0700

    implement concrete item factory method

--- app/src/main/java/com/cerealkillers/rootrunner/GameObjects/Items/ConcreteItemFactory.java
+++ app/src/main/java/com/cerealkillers/rootrunner/GameObjects/Items/ConcreteItemFactory.java
@@ -8,6 +8,24 @@ import org.andengine.extension.tmx.TMXObject;
 public class ConcreteItemFactory {
 
     public static Item itemFromTmxObject(TMXObject tmxObject) {
-        return null;
+
+        String itemType = tmxObject.getType();
+        boolean isStatic  = itemType.equals("interactive"); //otherwise it's a pickup type
+
+        ConcreteItem item = new ConcreteItem(tmxObject.getName(),
+                tmxObject.getTMXObjectProperty("description"),
+                tmxObject.getId(),
+                isStatic
+        );
+
+        /*
+            TODO: attach listeners and other crap to new items based on additional TMX properties
+             which aren't defined yet.
+             For example, interactive items will listen for certain tools being used against it.
+             Pickup items don't really need any special treatment.
+         */
+
+
+        return item;
     }
 }
