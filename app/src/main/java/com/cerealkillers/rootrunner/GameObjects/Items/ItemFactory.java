8172051eaccc72442621d9843eb0f2685b1a98af
Benjamin Daschel <coffeemaxed@gmail.com>
2015-05-24 18:25:40 -0700

    delegate item creation to sub-factories

--- app/src/main/java/com/cerealkillers/rootrunner/GameObjects/Items/ItemFactory.java
+++ app/src/main/java/com/cerealkillers/rootrunner/GameObjects/Items/ItemFactory.java
@@ -3,6 +3,7 @@ package com.cerealkillers.rootrunner.GameObjects.Items;
 import org.andengine.extension.tmx.TMXObject;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * Created by Benjamin Daschel on 5/21/15.
@@ -20,15 +21,21 @@ public class ItemFactory {
 
     public Item itemFromTmxObject(TMXObject tmxObject) {
 
-        String className = tmxObject.getTMXObjectProperty("class");
-        if(className != null){
-            try {
-                Class itemClass = Class.forName(className);
-                return  (Item) itemClass.newInstance();
-            } catch (ReflectiveOperationException e) {
-                e.printStackTrace();
-            }
+        /*
+            There are four basic types of in game items
+            - Interactive: cannot be picked up but can be operated on with certain tools
+            - Pickup: able to be picked up but not usable on their own
+            - Tool: able to be picked up and used against other game objects
+         */
+
+        String itemType = tmxObject.getType();
+
+        if (itemType.equals("tool")){ //invoke ToolFactory
+            return ToolFactory.itemfromTmxObject(tmxObject);
+        }else{ //Invoke ConcreteItemFactory
+            return ConcreteItemFactory.itemFromTmxObject(tmxObject);
         }
-        return null;
     }
+
+
 }
