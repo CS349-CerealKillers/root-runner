e0116c9b36b8ab75160f0983720a5b672305a817
navarrofrank <franciscon12@hotmail.com>
2015-05-27 11:23:07 -0700

    Camera changed to bound camera.

--- app/src/main/java/com/cerealkillers/rootrunner/scene/SceneFactory.java
+++ app/src/main/java/com/cerealkillers/rootrunner/scene/SceneFactory.java
@@ -3,14 +3,14 @@ package com.cerealkillers.rootrunner.scene;
 import com.cerealkillers.rootrunner.SceneManager;
 
 import org.andengine.engine.Engine;
-import org.andengine.engine.camera.Camera;
+import org.andengine.engine.camera.BoundCamera;
 
 /**
  * Created by Benjamin Daschel on 5/21/15.
  */
 public class SceneFactory {
 
-    public static BaseScene createScene(SceneManager.SceneType type, Engine engine, Camera camera){
+    public static BaseScene createScene(SceneManager.SceneType type, Engine engine, BoundCamera camera){
 
         BaseScene scene = null;
         switch (type){
