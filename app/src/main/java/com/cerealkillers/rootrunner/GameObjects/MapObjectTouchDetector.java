4dc95fca61e26e1f2ed2965b2fb400625d7d23e4
Benjamin Daschel <coffeemaxed@gmail.com>
2015-05-29 07:58:08 -0700

    implement collision detected listener as obeservable

--- app/src/main/java/com/cerealkillers/rootrunner/GameObjects/MapObjectTouchDetector.java
+++ app/src/main/java/com/cerealkillers/rootrunner/GameObjects/MapObjectTouchDetector.java
@@ -12,14 +12,32 @@ import java.util.List;
  */
 class MapObjectTouchDetector implements IUpdateHandler {
 
+    public interface CollisionDetectedListener{
+        public void onCollisionDetected(MapObject subject, List<MapObject> touching);
+    }
+
     private final Map mAttachedMap;
     private MapObject mMapObject;
     private List<MapObject> touchResults;
+    private List<CollisionDetectedListener> mCollisionDetectedListeners;
 
     public MapObjectTouchDetector(MapObject mapObject, Map attachedMap) {
         mMapObject = mapObject;
         mAttachedMap = attachedMap;
         touchResults = new ArrayList<>();
+        mCollisionDetectedListeners = new ArrayList<>();
+    }
+
+    public void registerCollisionListener(CollisionDetectedListener listener){
+        if(listener != null && ! mCollisionDetectedListeners.contains(listener)){
+            mCollisionDetectedListeners.add(listener);
+        }
+    }
+
+    public void unregisterCollistionListener(CollisionDetectedListener listener){
+        if(listener != null){
+            mCollisionDetectedListeners.remove(listener);
+        }
     }
 
     @Override
