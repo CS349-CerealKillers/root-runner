7110827ef8245103028e2b3f5667e5e314f03699
tyler_herrin@eagles.ewu.edu <tyler_herrin@eagles.ewu.edu>
2015-05-29 10:11:08 -0700

    Apparently my special behavior class never got added in to the repo.

---
+++ app/src/main/java/com/cerealkillers/rootrunner/GameObjects/Characters/SpecialBehavior.java
@@ -0,0 +1,9 @@
+package com.cerealkillers.rootrunner.GameObjects.Characters;
+
+/**
+ * Created by Tyler Herrin on 5/28/2015.
+ */
+public interface SpecialBehavior
+{
+    public void useSpecial();
+}
