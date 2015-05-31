1fa10ea29b651dd7fc150c1931841c52a8e7b957
Benjamin Daschel <coffeemaxed@gmail.com>
2015-05-30 19:39:02 -0700

    add player reference and spawn player method to world

--- app/src/main/java/com/cerealkillers/rootrunner/GameWorld/World.java
+++ app/src/main/java/com/cerealkillers/rootrunner/GameWorld/World.java
@@ -1,7 +1,11 @@
 package com.cerealkillers.rootrunner.GameWorld;
 
+import com.cerealkillers.rootrunner.GameObjects.MapObject;
+import com.cerealkillers.rootrunner.GameObjects.Player;
 import com.cerealkillers.rootrunner.SceneManager;
 import com.cerealkillers.rootrunner.scene.BaseScene;
+
+import java.util.List;
 
 /**
  * Created by Benjamin Daschel on 5/25/15.
@@ -14,6 +18,7 @@ public class World {
     private Map mCurrentMap;
     private MapLoader mMapLoader;
     private boolean initialized;
+    private Player mPlayer;
 
     public World(MapLoader mapLoader, SceneManager sceneManager){
         mMapLoader = mapLoader;
@@ -40,6 +45,20 @@ public class World {
         BaseScene gameScene = mSceneManager.getCurrentScene();
         mCurrentMap = mMapLoader.load(initialMap, gameScene);
 
-        //call onMapLoaded on the game scene to finish scene setup
+        spawnPlayer();
+    }
+
+    private void spawnPlayer(){
+        //replace the player spawn marker with the player's sprite
+
+        List<MapObject> potentialSpawns = mCurrentMap.findByTag("playerspawn");
+        if (potentialSpawns.size() < 0){
+            MapObject spawnPoint = potentialSpawns.get(0); //there should only ever be one
+            mCurrentMap.replaceMapObject(spawnPoint, mPlayer);
+        }
+    }
+
+    public void setPlayer(Player player) {
+        mPlayer = player;
     }
 }
