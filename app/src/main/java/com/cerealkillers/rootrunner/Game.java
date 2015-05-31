0a517653e68f66ae7564b868f8bd07f97cd04f33
Benjamin Daschel <coffeemaxed@gmail.com>
2015-05-30 19:39:56 -0700

    create player on game load and set player on world

--- app/src/main/java/com/cerealkillers/rootrunner/Game.java
+++ app/src/main/java/com/cerealkillers/rootrunner/Game.java
@@ -2,14 +2,17 @@ package com.cerealkillers.rootrunner;
 
 import android.content.Context;
 
+import com.cerealkillers.rootrunner.GameObjects.Player;
 import com.cerealkillers.rootrunner.GameWorld.MapLoader;
 import com.cerealkillers.rootrunner.GameWorld.MapLoaderFactory;
+import com.cerealkillers.rootrunner.GameWorld.PlayerHolder;
 import com.cerealkillers.rootrunner.GameWorld.World;
 import com.cerealkillers.rootrunner.scene.BaseScene;
 import com.cerealkillers.rootrunner.scene.SceneFactory;
 import com.cerealkillers.rootrunner.scene.SceneListener;
 
 import org.andengine.engine.Engine;
+import org.andengine.entity.sprite.Sprite;
 
 /**
  * Created by Benjamin Daschel on 5/25/15.
@@ -22,6 +25,7 @@ public class Game {
     private ResourceManager mResourceManager;
     private World mWorld;
     private SceneListener.SceneChangeListener<BaseScene> mGameSceneSceneChangeListener;
+    private Player mPlayer;
 
     public Game(Context context, Engine engine){
         mMapLoader = MapLoaderFactory.getMapLoader(context, engine);
@@ -29,6 +33,16 @@ public class Game {
         mEngine = engine;
         createResourceManager(context);
         createSceneManager();
+        createPlayer();
+    }
+
+    /**
+     * This method should be replaced eventually with a dynamic player selection screen.
+     * For now, who cares.
+     */
+    private void createPlayer() {
+        Sprite playerSprite = new Sprite(0, 0, mResourceManager.getPlayerTiledTextureRegion(), mEngine.getVertexBufferObjectManager());
+        mPlayer = new Player(0, playerSprite);
     }
 
     private class GameSceneListener implements SceneListener.SceneChangeListener<BaseScene>{
@@ -37,6 +51,7 @@ public class Game {
         public void onSceneLoaded(BaseScene scene) {
             //TODO: Don't really want to create a new world every time the scene changes
             mWorld = new World(mMapLoader, mSceneManager);
+            mWorld.setPlayer(mPlayer);
             mWorld.initialize();
         }
 
