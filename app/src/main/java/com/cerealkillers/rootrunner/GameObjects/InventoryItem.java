4284ee878744dc207409aa584f560348e3051b1e
Benjamin Daschel <coffeemaxed@gmail.com>
2015-06-03 11:08:10 -0700

    add inventory item class

---
+++ app/src/main/java/com/cerealkillers/rootrunner/GameObjects/InventoryItem.java
@@ -0,0 +1,13 @@
+package com.cerealkillers.rootrunner.GameObjects;
+
+import org.andengine.entity.sprite.Sprite;
+
+/**
+ * Created by Benjamin Daschel on 6/3/15.
+ */
+public class InventoryItem extends GameObject {
+
+    public InventoryItem(int id, Sprite sprite) {
+        super(id, sprite);
+    }
+}
