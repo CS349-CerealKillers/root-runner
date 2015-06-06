8b9e82bd9ce14d28d111f6b59d63d1cae04edebe
tyler_herrin@eagles.ewu.edu <tyler_herrin@eagles.ewu.edu>
2015-06-06 12:05:06 -0700

    trying to fix adduser name git issues

---
+++ app/src/main/java/com/cerealkillers/rootrunner/GameObjects/AddUser.java
@@ -0,0 +1,23 @@
+package com.cerealkillers.rootrunner.GameObjects;
+
+import android.util.Log;
+
+import com.cerealkillers.rootrunner.GameWorld.Map;
+
+import org.andengine.entity.sprite.Sprite;
+
+/**
+ * Created by Tyler Herrin on 6/4/2015.
+ */
+public class AddUser extends MapTool
+{
+    public AddUser(Sprite sprite)
+    {
+        super(sprite, "adduser", "Add a user to the system.");
+    }
+
+    public void use(Map map, float x, float y)
+    {
+        Log.d("ADDUSER", "detected adduser tool use");
+    }
+}
